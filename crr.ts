f7072e1074c1d8a95d305da24e67406842a34be0
Jori Lehtinen <lehtinenjori03@gmail.com>
2026-05-11 07:44:47 +0300

    Add Convergent Replicated Resource Snapshot types

---
+++ crr.ts
@@ -0,0 +1,35 @@
+import { CRMapSnapshot } from '@sovereignbase/convergent-replicated-map'
+import { CRSetSnapshot } from '@sovereignbase/convergent-replicated-set'
+import { CRStructSnapshot } from '@sovereignbase/convergent-replicated-struct'
+import {
+  CipherMessage,
+  OpaqueIdentifier,
+  VerifyKey,
+} from '@sovereignbase/cryptosuite'
+
+type ConvergentReplicatedResourceClearanceSnapshot = CRStructSnapshot<{
+  owner: CRMapSnapshot<OpaqueIdentifier, VerifyKey>
+  manager: CRMapSnapshot<OpaqueIdentifier, VerifyKey>
+  editor: CRMapSnapshot<OpaqueIdentifier, VerifyKey>
+}>
+
+type ConvergentReplicatedResourceSnapshotBase<
+  Kind extends 'private' | 'public',
+  Data,
+> = CRStructSnapshot<{
+  kind: Kind
+  data: Data
+  host: CRSetSnapshot<string>
+  clearance: ConvergentReplicatedResourceClearanceSnapshot
+  authorization: Base64URLString
+}>
+
+export type ConvergentReplicatedResourceSnapshotPrivate =
+  ConvergentReplicatedResourceSnapshotBase<'private', CipherMessage>
+
+export type ConvergentReplicatedResourceSnapshotPublic =
+  ConvergentReplicatedResourceSnapshotBase<'public', Array<SchemaCRDT>>
+
+export type ConvergentReplicatedResourceSnapshot =
+  | ConvergentReplicatedResourceSnapshotPrivate
+  | ConvergentReplicatedResourceSnapshotPublic
